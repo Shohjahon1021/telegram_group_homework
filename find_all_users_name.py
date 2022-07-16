efeef0cccd12266ecdf6ad93a14e80e4e27e303f
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2022-07-16 13:53:00 +0500

    find_all_users_name.py

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -9,4 +9,31 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    return
+    list1 = []
+    list2 = []
+    list_1 = []
+    messages = data["messages"]
+    for i in messages:
+        list1.append(i.get("actor",0))
+    for k in list1:
+         if k != 0:
+             list_1.append(k)
+
+    for j in messages:
+        list2.append(j.get("from",0))
+    for s in list2:
+         if s != 0:
+             list_1.append(s)
+    
+            
+                
+                
+        
+        # for e in i.keys():
+        #     if i == "actor" or "from":
+        #         list1.append(i[e])
+        #     print(list1)
+    return list_1
+
+a = read_data("data/result.json")
+print(find_all_users_name(a))
