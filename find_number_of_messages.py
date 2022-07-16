d7f35ce2680f005e154dd0d1a3871fcba874f60e
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2022-07-16 12:33:11 +0500

    find_number_of_messages.py

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -10,4 +10,7 @@ def find_number_of_messages(data: dict)->int:
         int: Total number of messages.
     
     """
-    return
+    return len(data)
+
+a = read_data("data/result.json")
+find_number_of_messages(a)
