3fcd4db41d914d1687f9187b0c0b20f454194e31
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2022-07-16 15:47:59 +0500

    find_all_users_name.py

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -15,18 +15,11 @@ def find_all_users_name(data: dict)->list:
     messages = data["messages"]
     for i in messages:
         list1.append(i.get("actor",0))
+        list1.append(i.get("from",0))
     for k in list1:
         if k != 0:
             if k not in list_1:
-                
                 list_1.append(k)
-
-    # for j in messages:
-    #     list2.append(j.get("from",0))
-    # for s in list2:
-    #     if s != 0:
-    #         if s not in list_1:s
-    #             list_1.append(s)
 
     return list_1
 
