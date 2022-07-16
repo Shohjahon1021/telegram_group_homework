17d63dd718dd04b8204a4009c95dbb86115bfa81
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2022-07-16 15:52:53 +0500

    find_all_users_id.py

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -10,22 +10,15 @@ def find_all_users_id(data: dict)->list:
         list: List containing all the users id
     """
     list1 = []
-    list2 = []
     list_1 = []
     messages = data["messages"]
     for i in messages:
         list1.append(i.get("actor_id",0))
+        list1.append(i.get("from_id",0))
     for k in list1:
          if k != 0:
             if k not in list_1:
                 list_1.append(k)
-
-    for j in messages:
-        list2.append(j.get("from_id",0))
-    for s in list2:
-        if s != 0:
-            if s not in list_1:
-                list_1.append(s)
     return list_1
 
 a = read_data("data/result.json")
