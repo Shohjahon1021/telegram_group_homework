91339308da5f2ee87988821a1ae06f8e949642bf
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2022-07-19 09:24:57 +0500

    find_user_message_count.py

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -11,14 +11,4 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    list1 = []
-    list2 = []
-    list_1 = []
-    messages = data["messages"]
-    for i in messages:
-        list1.append(i.get("text",0))
-        list1.append(i.get("question",0))
-    print(list1)
-    return 
-
-find_user_message_count(read_data("data/result.json")),find_user_message_count(find_all_users_id(read_data("data/result.json")))
+    
